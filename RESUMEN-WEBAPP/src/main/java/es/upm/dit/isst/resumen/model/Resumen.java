bda7c89df209619803596f073a347e51e6785d99
juanjarabarojo <47561721+juanjarabarojo@users.noreply.github.com>
2021-04-18 17:01:48 +0200

    Version 1.0 del proyecto Resumen.es
    Implementada la persistencia de datos y la parte de cliente pero no conectadas. Caso de uso Leer resumen implementado. Navegación básica de la aplicación.

--- RESUMEN-WEBAPP/src/main/java/es/upm/dit/isst/resumen/model/Resumen.java
+++ RESUMEN-WEBAPP/src/main/java/es/upm/dit/isst/resumen/model/Resumen.java
@@ -1,34 +1,18 @@
 package es.upm.dit.isst.resumen.model;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
 
-@Entity
+//Generar getters y setters, equals para id, Tambien añadir librerias de persistencia en el Maven.
 public class Resumen implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	@Id
 	private int id;
 	private String title; // No tiene por que ser único, ¿Puede haber varios resumenes del mismo libro?
 	private String author; // Nombre y apellidos mejor, no creo que se busque solo por nombre o apellidos el autor
-	@Lob
 	private byte[] document; //El documento en si, seguramente un pdf
-	@Lob 
-	private byte[] image;
 	//private String writter; //El autor del libro tambien lo podemos poner (poner get y set si se hace
-	//categoria
 	//Resto de atributos estan relacionados con autenticación se añadiran posteriormente
 	
-	public byte[] getImage() {
-		return image;
-	}
-
-	public void setImage(byte[] image) {
-		this.image = image;
-	}
-
 	public Resumen() {}
 	
 	public int getId() {
